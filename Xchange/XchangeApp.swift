bd5a1d84724caa805c7b8e4124bca9c179108fa1
Sanya1001 <saniasinha2003@gmail.com>
2024-01-20 19:21:05 -0500

    Initial Commit

---
+++ Xchange/XchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  XchangeApp.swift
+//  Xchange
+//
+//  Created by Sania Sinha on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct XchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
